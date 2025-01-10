f99cdd808e8f1159a531cf1ce9d271f96c2abef8
Alex Tao <taofengonly1@gmail.com>
2025-01-10 11:10:14 +0800

    fix: update regex to remove special characters in message processing

--- packages/client-discord/src/messages.ts
+++ packages/client-discord/src/messages.ts
@@ -972,7 +972,7 @@ export class MessageManager {
         );
 
         // strip all special characters
-        messageContent = messageContent.replace(/[^\p{L}\p{N}\s\-_./:?=&]/gu, "");
+        messageContent = messageContent.replace(/[^\p{L}\s]/gu, "") ;
 
         // short responses where eliza should stop talking and disengage unless mentioned again
         if (
